ea2e0697af515d0cfc1917b54bb788c9bce0f52e
AnyaScheinman <98228468+AnyaScheinman@users.noreply.github.com>
2025-02-13 13:22:51 -0500

    Created Screens, Login, Register, number screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Login } from './Login';
+// import Numbers1 from './Numbers1';
+import NumberImages from './numberImages';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NumberImages />
   );
 }
 
