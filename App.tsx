d5bd08c9f64a22d2b595700bbb67be5a8f3e345d
thesomeoldguy <102364953+thesomeoldguy@users.noreply.github.com>
2025-02-13 21:27:54 -0500

    Fix import error for navigation

--- App.tsx
+++ App.tsx
@@ -1,25 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Login } from './Login';
- //import Numbers1 from './Numbers1';
-//import NumberImages from './numberImages';
-import LinkPage from './linkPage';
+import { StyleSheet} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NavigationProp } from "@react-navigation/native";
+import LinkPage from "./linkPage";
 
-
-
-
+type RootStackParamList = {
+  LinkPage: undefined;
+};
 
 export default function App() {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   return (
-    <LinkPage />
+      <LinkPage navigation={navigation} />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
