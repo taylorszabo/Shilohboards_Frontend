8df3ede84aa257eed7fee25015db152b1d51d64e
aBielaski <102336048+aBielaski@users.noreply.github.com>
2025-03-17 17:26:39 -0400

    issue fixed and script cleaned up

--- app/PerformanceReports.tsx
+++ app/PerformanceReports.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
 import { useState, useEffect } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import CustomButton from '../reusableComponents/CustomButton';
-import { useRouter, useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams } from 'expo-router';
 import BackgroundLayout from '../reusableComponents/BackgroundLayout';
 import { tempCharacterArray } from "../CharacterOptions";
 import PerformanceBar from '../reusableComponents/PerformanceBar';
@@ -16,44 +16,34 @@ type ReportQuery = {
 }
 
 export default function SelectCharacter() {
-  const router = useRouter();
   const { playerId = '[name]' } = useLocalSearchParams();
   const windowHeight = useWindowDimensions().height;
-
   const [query, setQuery] = useState<ReportQuery>({playerId: 0, game: gamesArray[0].title, level: 1});
-
   const middleIndex = Math.ceil(query.game === gamesArray[0].title ? alphabetArray.length / 2 : numbersArray.length / 2);
 
+  //----------------------------------------------------------
   useEffect(() => {
-    console.log('--useEffect---')
-      console.log(query.level);
-      }, [query.level]);
+    //fetch with new params
+  }, [query]);
 
+  //----------------------------------------------------------
   function setGameSelected(game: string) {
-    //if level 3 is selected/underlined when alphabet game is chosen and then you switch to the numbers game,
-    //set the level to 2 since level 3 doesn't exist
-    console.log('---------------');
-    // console.log(query.game);
-    // console.log(query.level);
-    // console.log(gamesArray[1].title);
-    // console.log(gamesArray[0].numberOfLevels);
-    // console.log(query.game === gamesArray[1].title);
-    // console.log(query.level === gamesArray[0].numberOfLevels);
+    //if level 3 is selected/underlined while the alphabet game is chosen and then you switch to the numbers game,
+    //set the level to 2 since level 3 doesn't exist in the numbers game
     if (query.game === gamesArray[0].title && query.level === gamesArray[0].numberOfLevels) {
-      console.log('execute');
-      //console.log(gamesArray[1].numberOfLevels);
-      setQuery({...query, level: 2});
+      setQuery({...query, level: 2, game: game});
+      return;
     }
     console.log('anyway');
     setQuery({...query, game: game});
-    
   }
 
+  //----------------------------------------------------------
   return (
     <BackgroundLayout>
         <View style={[styles.container, { minHeight: Math.round(windowHeight) }]}>
             <View  style={styles.header}>
-              {/* =============== Back Button =============== */}
+              {/* Back Button */}
               <CustomButton image={require('../assets/back.png')} uniqueButtonStyling={styles.backBtnContainer} onPressRoute={`/MainMenu?&playerId=${playerId}`}/>
 
               <Text style={styles.headerText}>Performance Reports</Text>
@@ -65,7 +55,8 @@ export default function SelectCharacter() {
             <LinearGradient colors={['#E1CEB6', 'rgba(0, 0, 0, 0)']} style={styles.selectionBars}>
                 {[...tempCharacterArray].map((user, index) => (
                     <View key={user.id}>
-                        <Text key={user.id} style={[styles.bodyText, user.id === query.playerId && styles.selectedUnderline]} onPress={() => setQuery({...query, playerId: user.id})}>
+                        <Text key={user.id} style={[styles.bodyText, user.id === query.playerId && styles.selectedUnderline]} 
+                              onPress={() => setQuery({...query, playerId: user.id})}>
                           {user.name}
                         </Text>
                     </View>
@@ -76,7 +67,8 @@ export default function SelectCharacter() {
             <LinearGradient colors={['#E1CEB6', 'rgba(0, 0, 0, 0)']} style={styles.selectionBars}>
                 {[...gamesArray].map((game, index) => (
                     <View key={index}>
-                        <Text style={[styles.bodyText, game.title === query.game && styles.selectedUnderline]} onPress={() => setGameSelected(game.title)}>
+                        <Text style={[styles.bodyText, game.title === query.game && styles.selectedUnderline]} 
+                              onPress={() => setGameSelected(game.title)}>
                           {game.title}
                         </Text>
                     </View>
@@ -87,7 +79,8 @@ export default function SelectCharacter() {
             <LinearGradient colors={['#E1CEB6', 'rgba(0, 0, 0, 0)']} style={styles.selectionBars}>
                 {[...Array(gamesArray[query.game === gamesArray[0].title ? 0 : 1].numberOfLevels)].map((level, index) => (
                     <View key={index}>
-                        <Text style={[styles.bodyText, index + 1 === query.level && styles.selectedUnderline]} onPress={() => setQuery({...query, level: index + 1})}>
+                        <Text style={[styles.bodyText, index + 1 === query.level && styles.selectedUnderline]} 
+                              onPress={() => setQuery({...query, level: index + 1})}>
                           Level {index + 1}
                         </Text>
                     </View>
@@ -123,7 +116,7 @@ export default function SelectCharacter() {
 
             </View>
 
-            <Text style={[styles.bodyText, {textDecorationLine: 'underline', textAlign: 'center', paddingTop: 0, paddingBottom: 20, fontSize: 16}]}>Click here for game & level descriptions if needed</Text>
+            <Text style={[styles.bodyText, styles.gameInstructionLink]}>Click here for game & level descriptions if needed</Text>
         </View>
     </BackgroundLayout>
   );
@@ -134,15 +127,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     height: '100%'
-    //backgroundColor: 'red'
   },
   header: {
     flexDirection: 'row'
   },
   backBtnContainer: {
-    // position: 'absolute',
-    // top: 0,
-    // left: 0,
     paddingVertical: 20
   },
   headerText: {
@@ -153,7 +142,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 26,
     color: '#3E1911',
-    //backgroundColor: 'blue'
   },
   bodyText: {
     verticalAlign: 'middle',
@@ -162,7 +150,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     color: '#3E1911',
-    //backgroundColor: 'blue'
   },
   selectionBars: {
     borderBottomWidth: 4,
@@ -176,7 +163,6 @@ const styles = StyleSheet.create({
     padding: 20,
     paddingTop: 0,
     columnGap: '10%',
-    //backgroundColor: 'blue'
   },
   arrayItem: {
     flexDirection: 'row', 
@@ -193,5 +179,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold', 
     color: '#3E1911',
     fontSize: 20
+  },
+  gameInstructionLink: {
+    textDecorationLine: 'underline', 
+    textAlign: 'center', 
+    paddingTop: 0, 
+    paddingBottom: 20, 
+    fontSize: 16
   }
 });
