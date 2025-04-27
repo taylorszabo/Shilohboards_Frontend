e2ea715db44c89f7f471d4bacd60bdb319f78d55
aBielaski <102336048+aBielaski@users.noreply.github.com>
2025-04-27 19:01:47 -0400

    login page pretty much done for web now

--- app/Login.tsx
+++ app/Login.tsx
@@ -107,23 +107,18 @@ export default function Login() {
           />
         )}
 
-        {/* Password info bubble */}
-        {showPasswordInfo && (
-          <View style={styles.bubble}>
-            <Text style={styles.bubbleText}>• Password must be at least 6 characters.</Text>
-            <Text style={styles.bubbleText}>• Must contain at least 1 number</Text>
-          </View>
-        )}
-
-        <Image
-          source={require("../assets/logo.png")}
-          style={[styles.logo, { width: width * 0.5, height: width * 0.5 }]}
-        />
+        <View style={{width: '90%', height: '30%', alignItems: 'center', justifyContent: 'center'}}>
+          <Image
+            source={require("../assets/logo.png")}
+            style={[styles.logo]}
+          />
+        </View>
+        
         {fromSettings === "true" ? <Text style={styles.title}>Confirm Old Email and Password</Text>: <Text style={styles.title}>Login</Text>}
 
 
         <TextInput
-          style={[styles.input, { width: width * 0.8, fontSize: width * 0.045 }]}
+          style={[styles.input, { width: '80%', fontSize: 20, marginBottom: 15, minWidth: '80%' }, width > 800 && {minWidth: 450 }]}
           placeholder="Email"
           value={email}
           onChangeText={setEmail}
@@ -131,9 +126,9 @@ export default function Login() {
           keyboardType="email-address"
         />
 
-        <View style={{ width: width * 0.8, position: "relative", marginBottom: height * 0.015 }}>
+        <View style={[{ width: '80%', position: "relative", justifyContent: "center", alignItems: 'center', minWidth: '80%'}, width > 800 && {minWidth: 450 }]}>
           <TextInput
-            style={[styles.input, { fontSize: width * 0.045 }]}
+            style={[styles.input, { fontSize: 20, width: '100%', minWidth: '100%' }]}
             placeholder="Password"
             secureTextEntry
             value={password}
@@ -144,8 +139,16 @@ export default function Login() {
             style={styles.infoIcon}
             onPress={() => setShowPasswordInfo(prev => !prev)}
           >
-            <Text style={{ fontSize: 18 }}>ℹ️</Text>
+            <Text style={{ fontSize: 18, marginVertical: 'auto'}}>ℹ️</Text>
           </TouchableOpacity>
+
+          {/* Password info bubble */}
+          {showPasswordInfo && (
+            <View style={styles.bubble}>
+              <Text style={styles.bubbleText}>• Password must be at least 6 characters.</Text>
+              <Text style={styles.bubbleText}>• Must contain at least 1 number</Text>
+            </View>
+          )}
         </View>
 
         {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
@@ -156,7 +159,7 @@ export default function Login() {
   <CustomButton
     text="Sign In"
     functionToExecute={handleLogin}
-    uniqueButtonStyling={{ width: width * 0.6, height: height * 0.08 }}
+    uniqueButtonStyling={{ width: '55%', marginTop: 30, minWidth: 200 }}
   />
 )}
         <TouchableOpacity onPress={() => router.push("/ResetPassword")}>
@@ -176,77 +179,69 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    maxWidth: 700,
     justifyContent: "center",
     alignItems: "center",
-    paddingBottom: height * 0.08,
+    paddingBottom: 40,
+    margin: 'auto'
   },
   logo: {
+    width: '100%',
+    aspectRatio: 1,
     resizeMode: "contain",
   },
   title: {
-    fontSize: width * 0.08,
+    fontSize: 33,
     fontWeight: "700",
     color: "#3E1911",
-    marginBottom: height * 0.04,
+    margin: 25,
     textAlign: "center",
   },
   input: {
-    height: height * 0.07,
+    position: 'relative',
     backgroundColor: "#fff",
     borderRadius: 10,
+    paddingVertical: 10,
     paddingHorizontal: 12,
-    marginBottom: height * 0.015,
     borderWidth: 1,
     borderColor: "#494649",
     shadowColor: "rgba(0, 0, 0, 0.25)",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 1,
     shadowRadius: 4,
-    zIndex: 1,
   },
   errorText: {
     color: "red",
     marginBottom: 10,
   },
-  button: {
-    backgroundColor: "#C3E2E5",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 8,
-    marginTop: height * 0.02,
-    borderRightWidth: 2,
-    borderBottomWidth: 3,
-    borderColor: "#A9A9A9",
-  },
-  buttonText: {
-    color: "#3E1911",
-    fontSize: width * 0.06,
-    fontWeight: "700",
-    textAlign: "center",
-  },
   registerText: {
     marginTop: height * 0.03,
     color: "#3E1911",
-    fontSize: width * 0.05,
+    fontSize: 18,
     fontWeight: "400",
     textDecorationLine: "underline",
   },
   infoIcon: {
     position: "absolute",
     right: 10,
-    top: height * 0.02,
+    top: 0,
+    bottom: 0,
     zIndex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 'auto',
   },
   bubble: {
     position: "absolute",
-    top: height * 0.32, // adjust to match password input location
-    right: width * 0.1,
+    top: '50%',
+    transform: [{ translateY: -90 }],
+    right: 0,
+    left: 0,
     backgroundColor: "#fff",
     padding: 10,
     borderRadius: 10,
     borderWidth: 2,
     borderColor: "#2aa0b8",
-    width: width * 0.8,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
@@ -255,7 +250,7 @@ const styles = StyleSheet.create({
     elevation: 10,
   },
   bubbleText: {
-    fontSize: width * 0.035,
-    color: "#333",
+    fontSize: 15,
+    color: "#3E1911",
   },
 });
